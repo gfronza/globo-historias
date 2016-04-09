1f104e23697590b2c153297999b4c0e64afacb95
Germano Fronza <germano@eventials.com>
2016-04-09 18:26:53 -0300

    Story save done

--- api/server.js
+++ api/server.js
@@ -1,30 +1,23 @@
 var express = require('express');
-var fileUpload = require('express-fileupload');
-var util = require('util');
+var multer = require('multer');
+var repository = require('./repository');
 var app = express();
 
-app.use(fileUpload());
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './storage');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now() + '.mp4');
+  }
+});
+
+var storyVideoUpload = multer({ storage : storage}).single('storyVideo');
 
 app.route('/composer/create-new-story')
     .post(function(req, res) {
-        if (!req.files) {
-    		res.json({
-                error: 'No file was uploaded'
-            });
-    		return;
-    	}
-
-        // TODO: extract constant.
-        videoStorageDir = '/Users/germano/Pessoal/MyDev/hackathon/hackathon-globo/storage'
-
-        var storyVideo = req.files.storyVideo;
-
-        // a very lame name for now.
-        var timestamp = new Date().getTime().toString(),
-            videoInStorage = util.format('%s/%s_%d', videoStorageDir, storyVideo.name, timestamp);
-
-    	storyVideo.mv(videoInStorage, function(err) {
-    		if (err) {
+        storyVideoUpload(req, res, function(err) {
+            if (err) {
                 res.json({
                     error: 'Video could not be moved to the storage.'
                 });
@@ -33,7 +26,7 @@ app.route('/composer/create-new-story')
                 var story = repository.createStory(req.body);
                 res.json(story);
     		}
-    	});
+        });
     });
 
 app.listen(3000);
