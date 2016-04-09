0564a6978bf43615c45a67f46b0d76090c9e0193
gabrielsina <gabriel.sina@gmail.com>
2016-04-09 18:47:18 -0300

    endpoints get-activities and get-stories-by-activity-id

--- api/server.js
+++ api/server.js
@@ -29,6 +29,33 @@ app.route('/composer/create-new-story')
         });
     });
 
+  app.route('/editor/get-activities')
+    .get(function(req, res){
+      //call the database to get the activities
+      //insert the following code as callback
+      var err = false;
+      if(err){
+        console.log(err);
+        res.send(err);
+      } else {
+        var response = fake.FAKE_ACTIVITIES;
+        res.send(response);
+      }
+  });
+
+  app.get('/editor/get-stories-by-activity-id', function(req, res){
+    if (!req.query.activity_id) res.send({failure: 'activity_id was not send'});
+    var activity_id = req.query.activity_id;
+    //call the database to retrieve stories with matched_activities._id == activity_id
+    var response = fake.apiHelper('stories-by-activity', {activity_id});
+    if(response.err){
+      console.log(err);
+      res.send(err);
+    } else {
+      res.send(response);
+    }
+  });
+
 app.listen(3000);
 
 console.log("Server is running on port 3000");
