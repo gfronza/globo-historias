bb01768ef5b6043c89f100c97ce34e6c8a28e81f
Germano Fronza <germano@eventials.com>
2016-04-10 08:10:44 -0300

    Fix story persist

--- api/videoManager.js
+++ api/videoManager.js
@@ -5,8 +5,8 @@ module.exports = {
         var ts = new Date().getTime().toString();
         var audioFilename = '/vagrant/storage/a_' + ts + '.wav';
         var videoFilename = '/vagrant/storage/v_' + ts + '.webm';
-        var finalVideo = '/vagrant/storage/av_' + ts + '.mp4'
-        var videoThumb = '/vagrant/storage/tb_' + ts + '.png'
+        var finalVideo = '/vagrant/storage/av_' + ts + '.mp4';
+        var videoThumb = '/vagrant/storage/tb_' + ts + '.png';
 
         var audioPromise = recording.saveRecording(blobAudio, audioFilename);
         var videoPromise = recording.saveRecording(blobVideo, videoFilename);
@@ -19,6 +19,9 @@ module.exports = {
             });
         });
 
-        return finalVideo;
+        return {
+            videoFilename: 'av_' + ts + '.mp4',
+            videoThumb: 'tb_' + ts + '.png'
+        }
     }
 }
