851c0b8e971d2b33d27d562e2e7b9b0d955bd833
gabrielsina <gabriel.sina@gmail.com>
2016-04-10 09:34:39 -0300

    fixed local conflicts

--- api/repository.js
+++ api/repository.js
@@ -139,60 +139,20 @@ module.exports = {
         }
       };
     },
-    storiesByAuthor: function(author_id) {
-      return [
-        {
-          _id: 283239819393,
-          author: {
-            id: 321321321,
-            name: 'Gabriel',
-            snapshot_url: 'foto_do_user.jpg'
-          },
-          video_url: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4',
-          story_url: 'address-in-which-the-video-will-be-available',
-          story_thumb: 'http://tinyurl.com/jood6zl',
-          tags: [],
-          time_stamp: '2016-11-04 10:51:00',
-          matched: true,
-          matched_activities: [
-            {activity_id: 321362138921973, approved: true}
-          ],
-          banned: true,
-          reviewed: false,
-          reactions: {
-            like: 4,
-            sad: 2,
-            love: 3,
-            angry: 2,
-            wow: 0
-          }
-      },
-      {
-        _id: 283239819393,
-        author: {
-          id: 321321321,
-          name: 'Gabriel',
-          snapshot_url: 'foto_do_user.jpg'
-        },
-        video_url: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4',
-        story_url: 'address-in-which-the-video-will-be-available',
-        story_thumb: 'http://tinyurl.com/jood6zl',
-        tags: [],
-        time_stamp: '2016-11-04 10:51:00',
-        matched: true,
-        matched_activities: [
-          {activity_id: 321362138921973, approved: true}
-        ],
-        banned: true,
-        reviewed: false,
-        reactions: {
-          like: 4,
-          sad: 2,
-          love: 3,
-          angry: 2,
-          wow: 0
-        }
-      }
-    ];
-  }
+    getStoriesByAuthor: function(author_id) {
+        return new Promise(function (fulfill, reject) {
+            createConnection(function(err, db) {
+                db.collection('stories').find({'author.id': 312213213213}).toArray(function(err, stories) {
+                    if (err) {
+                        reject(err);
+                    }
+                    else {
+                        fulfill(stories);
+                    }
+
+                    db.close();
+                });
+            });
+        });
+    }
 }
